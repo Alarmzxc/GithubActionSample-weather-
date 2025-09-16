4be12cf52ec636807fef811a9e92801ffe1d3a28
Alarmzxc <Jgzxc@outlook.com>
2025-09-16 18:15:30 +0800

    每日一言API更新

--- weather_report.py
+++ weather_report.py
@@ -70,7 +70,7 @@ def get_access_token():
 
 def get_daily_love():
     # 每日一句情话
-    url = "https://api.lovelive.tools/api/SweetNothings/Serialization/Json"
+    url = "https://v1.hitokoto.cn/?c=i&encode=json"
     r = requests.get(url)
     all_dict = json.loads(r.text)
     sentence = all_dict['returnObj'][0]
